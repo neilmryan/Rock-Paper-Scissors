f364f8d1e749aee582a1bc8ba0674a396d422aa9
Neil <neilmarkoliverryan@gmail.com>
2019-07-30 22:19:55 -0700

    JS updates round resul to the DOM

--- action.js
+++ action.js
@@ -1,10 +1,9 @@
-alert("JS is working!");
-
+//alert("JS is working!");
+// array to store computer play options
 var computerPlays = ["rock", "paper", "scissors"];
 
 //computerPlay randomly returns either Rock, Paper or Scissors
 function computerPlay() {
-    
     //random number gets a number: 1,2,3
     var randomNumber = Math.floor(Math.random() * 3);
     var randomChoice = computerPlays[randomNumber];
@@ -15,9 +14,9 @@ function computerPlay() {
 
 //returns the outcome based on inputs from human and computer players
 function singleRound(playerSelection, computerSelection) {
-    
+
     playerSelection = playerSelection.toLowerCase();
-    
+
     if (playerSelection === computerSelection) {
         return "Tie!"
     } else {
@@ -51,8 +50,24 @@ function singleRound(playerSelection, computerSelection) {
 
 function game() {
   for (var i = 0; i < 10; i++) {
-  var playerChoice = prompt("Rock, Paper, or Scissors?");  
+  var playerChoice = prompt("Rock, Paper, or Scissors?");
   console.log(singleRound(playerChoice, computerPlay()));
   console.log(i + 1);
   }
+}
+
+const results = document.querySelector(".results");
+const round_result = document.createElement('p');
+
+player_choices = document.getElementsByClassName("player_choices");
+
+for (let i = 0; i < player_choices.length; i++) {
+  player_choices[i].addEventListener('click', function(e) {
+    let choice = e.target.id;
+    //console.log(choice);
+    let outcome = singleRound(choice, computerPlay());
+    console.log(outcome);
+    round_result.textContent = outcome;
+    results.append(round_result);
+  })
 }
