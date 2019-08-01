4d1a1d4518decf8c17c1183a619b6e4a66fae12a
Neil <neilmarkoliverryan@gmail.com>
2019-07-31 21:10:05 -0700

    Game functioning in the DOM per round

--- action.js
+++ action.js
@@ -1,73 +1,77 @@
 //alert("JS is working!");
+
 // array to store computer play options
-var computerPlays = ["rock", "paper", "scissors"];
+var computerPlays = ["Rock", "Paper", "Scissors"];
 
 //computerPlay randomly returns either Rock, Paper or Scissors
 function computerPlay() {
-    //random number gets a number: 1,2,3
-    var randomNumber = Math.floor(Math.random() * 3);
-    var randomChoice = computerPlays[randomNumber];
-    //console.log(randomNumber);
-    //console.log(randomChoice);
-    return randomChoice;
+  //random number gets a number: 1,2,3
+  var randomNumber = Math.floor(Math.random() * 3);
+  var randomChoice = computerPlays[randomNumber];
+  return randomChoice;
 }
 
 //returns the outcome based on inputs from human and computer players
 function singleRound(playerSelection, computerSelection) {
+  let winner = "";
 
-    playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
-        return "Tie!"
-    } else {
-          switch(playerSelection) {
-          case "rock":
-            if (computerSelection === "paper") {
-              return "You Lose!, Paper beats Rock.";
-            } else {
-              return "You Win! Rock beats Scissors.";
-            }
-          //break;
-          case "paper":
-            if (computerSelection === "scissors") {
-              return "You Lose!, Scissors beats Paper."
-            } else {
-              return "You Win! Paper beats Rock."
-            }
-          //break;
-          case "scissors":
-            if (computerSelection === "rock") {
-              return "You Lose!, Rock beats Scissors."
-            } else {
-              return "You Win! Scissors beats Paper."
-            }
-          //break;
-          default:
-            console.log("Invalid input.");
-    }
+  if (playerSelection === computerSelection) {
+    winner = "No one";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    winner = "Computer";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    winner = "Player";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    winner = "Computer";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    winner = "Player";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    winner = "Player";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    winner = "Computer";
   }
+  return winner;
 }
 
-function game() {
+
+function game(num_of_rounds) {
   for (var i = 0; i < 10; i++) {
-  var playerChoice = prompt("Rock, Paper, or Scissors?");
-  console.log(singleRound(playerChoice, computerPlay()));
-  console.log(i + 1);
+    var playerChoice = prompt("Rock, Paper, or Scissors?");
+    console.log(singleRound(playerChoice, computerPlay()));
+    console.log(i + 1);
   }
 }
 
 const results = document.querySelector(".results");
-const round_result = document.createElement('p');
+const pc = document.getElementById('#pc');
+const cc = document.getElementById('#cc');
+const whp = document.getElementById('#whp');
+const rr = document.getElementById('#rr');
+const choice_relationship = document.createElement('p');
 
-player_choices = document.getElementsByClassName("player_choices");
+const player_choices = document.getElementsByClassName("player_choices");
+let win_how_phrase = "";
 
 for (let i = 0; i < player_choices.length; i++) {
   player_choices[i].addEventListener('click', function(e) {
-    let choice = e.target.id;
-    //console.log(choice);
-    let outcome = singleRound(choice, computerPlay());
+
+    let player_choice = e.target.id;
+    pc.textContent = "Player Choice: " + player_choice + ".";
+    let computer_choice = computerPlay();
+    cc.textContent = "Computer Choice: " + computer_choice + ".";
+
+    let outcome = singleRound(player_choice, computer_choice);
     console.log(outcome);
-    round_result.textContent = outcome;
-    results.append(round_result);
+
+    if (outcome === "No one") {
+      win_how_phrase = "" + computer_choice + " ties with " + player_choice +".";
+    } else if (outcome === "Computer") {
+      win_how_phrase = "" + computer_choice + " beats " + player_choice +".";
+    } else {
+      win_how_phrase = "" + player_choice + " beats " + computer_choice +".";
+    }
+
+    whp.textContent = "" + win_how_phrase + "";
+    rr.textContent = "" + outcome + " wins this round!";
   })
 }
