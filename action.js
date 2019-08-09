a46f075052be8074c8baee9da26e6e622d316762
Neil Ryan <nr110718@bloomenergy.com>
2019-08-09 16:54:10 -0700

    Game is working with cartoon images but needs layout revision

--- action.js
+++ action.js
@@ -55,6 +55,9 @@ const choice_relationship = document.createElement('p');
 const p_total = document.getElementById('#player_total');
 const c_total = document.getElementById('#computer_total');
 const t_total = document.getElementById('#ties');
+let pc_image = document.getElementById('pc_img');
+let cc_image = document.getElementById('cc_img');
+//let winner_image = document.getElementById('gr_img');
 
 const player_choices = document.getElementsByClassName("player_choices");
 let win_how_phrase = "";
@@ -71,12 +74,20 @@ for (let i = 0; i < player_choices.length; i++) {
     let outcome = singleRound(player_choice, computer_choice);
     console.log(outcome);
     console.log(round_total);
+    console.log(player_choice);
+    console.log(computer_choice);
 
     if (outcome === "No one") {
+      pc_image.setAttribute('src', 'images/w_' + player_choice + '.png');
+      cc_image.setAttribute('src', 'images/w_' + computer_choice + '.png');
       win_how_phrase = "" + computer_choice + " ties with " + player_choice + ".";
     } else if (outcome === "Computer") {
+      pc_image.setAttribute('src', 'images/l_' + player_choice + '.png');
+      cc_image.setAttribute('src', 'images/w_' + computer_choice + '.png');
       win_how_phrase = "" + computer_choice + " beats " + player_choice + ".";
     } else {
+      pc_image.setAttribute('src', 'images/w_' + player_choice + '.png');
+      cc_image.setAttribute('src', 'images/l_' + computer_choice + '.png');
       win_how_phrase = "" + player_choice + " beats " + computer_choice + ".";
     }
 
@@ -97,8 +108,10 @@ for (let i = 0; i < player_choices.length; i++) {
       document.getElementById("Scissors").disabled = true;
       if (player_score > computer_score) {
         game_winner = "Player";
+        //winner_image.setAttribute('src', '');
       } else {
         game_winner = "Computer";
+        //winner_image.setAttribute('src', '');
       }
       rr.textContent = "" + game_winner + " wins the game!";
       rr.classList.add("blinking");
